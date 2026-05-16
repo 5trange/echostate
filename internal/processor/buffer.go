350ea10faf74295003badbc2da8405ca2259dbc4
5trange <64513428+5trange@users.noreply.github.com>
2026-05-17 01:15:41 +0530

    Ringbuffer struct overhaul

--- internal/processor/buffer.go
+++ internal/processor/buffer.go
@@ -1,5 +1,29 @@
 // internal/processor/buffer.go
 package processor
 
-type WifiDataBuffer struct {
+type RSSIRingBuffer struct {
+	rssiReading []int
+	size        int
+	head        int
+	count       int
+}
+
+func NewRSSIRingBuffer(size int) *RSSIRingBuffer {
+	return &RSSIRingBuffer{
+		rssiReading: make([]int, size),
+		size:        size,
+		head:        0, // Start from index 0
+		count:       0,
+	}
+}
+
+func (b *RSSIRingBuffer) Insert(rssi int) {
+	b.rssiReading[b.head] = rssi
+
+	// (head + 1) % size wraps around to 0 when head reaches the end
+	b.head = (b.head + 1) % b.size
+
+	if b.count < b.size {
+		b.count++
+	}
 }
